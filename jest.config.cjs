974b6d2d82a84054c9ae662ee7697fbb70ac2640
Xavier <xherriot@protonmail.com>
2022-06-29 17:05:04 +0200

    core(test): remove helpers/OpenTelemetry from coverage until we are looking to see how to test it

--- jest.config.cjs
+++ jest.config.cjs
@@ -15,6 +15,6 @@ module.exports = {
   },
   coveragePathIgnorePatterns: [
     '<rootDir>/node_modules/',
-    '<rootDir>/src/OpenTelemetry.js', // None way to tst it at this time
+    '<rootDir>/src/OpenTelemetry.ts', // None way to tst it at this time
   ],
 };
