591232b125b08ad9bf01d6d53debe9daa6555c8f
Xavier <xherriot@protonmail.com>
2022-07-19 13:04:41 +0200

    core(jest): optim conf

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,12 +5,6 @@ module.exports = {
   roots: [
     './src/',
   ],
-  extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
